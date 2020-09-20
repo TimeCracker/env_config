31b994836de90ec0aedd727add657466559dea3a
panyunpeng <panyunpeng@yy.com>
2020-09-20 20:48:19 +0800

    zsh neovim v0.1 debug_2

--- zsh_conf.sh
+++ zsh_conf.sh
@@ -3,3 +3,4 @@ cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 #git clone --depth=1 https://gitee.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 #cat ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc
+sed 's/ZSH_THEME=\"robbyrussell\"/ZSH_THEME=\"powerlevel10k/powerlevel10k\"' ~/.zshrc
