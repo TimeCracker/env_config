b1fb7a89120cbad8f26bca397c87ce9747cce44a
panyunpeng <panyunpeng@yy.com>
2020-09-20 21:58:09 +0800

    remove zsh and neovim before install

--- neovim_install.sh
+++ neovim_install.sh
@@ -1,3 +1,4 @@
+sudo apt-get --purge remove neovim
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt-get update
 sudo apt-get install neovim
--- zsh_install.sh
+++ zsh_install.sh
@@ -1,3 +1,4 @@
+sudo apt-get --purge remove zsh
 sudo apt-get update && sudo apt-get -y install zsh
 sudo chsh $USER -s $(which zsh)
 
