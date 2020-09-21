b781791e74f136193f6a7be923c9e70b1794a0b6
panyunpeng <panyunpeng@yy.com>
2020-09-21 17:06:14 +0800

    hadoop config

--- setup.sh
+++ setup.sh
@@ -41,6 +41,8 @@ else
     cp -rf ${cur_script_dir}/coc ${new_home}/.config
 fi
 
+bash add_alias.sh
+
 echo "Now you need to run: zsh -> source ~/.zshrc -> :PlugInstall in nvim"
 echo "Now you need to run: zsh -> source ~/.zshrc -> :PlugInstall in nvim"
 echo "Now you need to run: zsh -> source ~/.zshrc -> :PlugInstall in nvim"
